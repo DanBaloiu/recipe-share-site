5a643b89994cb3008a43740eed3277c3e52ac7d2
danbaloiu <dan_baloiu@yahoo.com>
2025-10-13 03:45:49 +0100

    chore(auth): redirect login/logout to home

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -203,6 +203,10 @@ else:
 
 WHITENOISE_USE_FINDERS = True
 
+# Redirect after login/logout (avoid default /accounts/profile/)
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
